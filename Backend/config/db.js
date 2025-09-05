1b9f23a59933009a5dc8f32d87d1d18740ed5352
ankitspandey36 <ankit.pandey99p@gmail.com>
2025-09-05 13:36:43 +0530

    database

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,13 +1,17 @@
-const mysql=require('mysql2/promise');
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config()
 
-const mySqlPool=mysql.createPool({
-  host: 'localhost',
-    user: 'root',      
-    password: 'HelloWorld',  
-    database: 'nationalbank' ,
-    
-  });
-  module.exports=mySqlPool;
-  
+const mySqlPool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB,
+  port: process.env.DB_PORT, 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+module.exports = mySqlPool;
 
 
